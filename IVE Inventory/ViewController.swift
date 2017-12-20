20d3576a6bf96eb5821438ac99bb48f9bb7ccdbc
Er Sanjay Morya <singh.avaneesh96@gmail.com>
2017-12-20 09:24:20 +0530

    Initial Commit

---
+++ IVE Inventory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IVE Inventory
+//
+//  Created by Er Sanjay Morya on 20/12/17.
+//  Copyright © 2017 Sanjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
